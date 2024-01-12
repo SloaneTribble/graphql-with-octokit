10c7a6ac730cb7c068fa95a8877635174e53aaab
Sloane Tribble <sloane.tribble@liatrio.com>
2024-01-12 12:52:16 -0800

    Successfully access repo id

--- create-pr-and-issue.js
+++ create-pr-and-issue.js
@@ -1,46 +1,36 @@
-const { Octokit } = require('@octokit/core');
+const mutate = require('./mutate');
 
-const createRepository = require('./create-repo');
-
-// contains personal access token
-const config = require('./config');
-
-const octokit = new Octokit();
-
-async function createIssue(repoId) {
-    // first half is request fields (name, etc.), second half indicates response fields (id, name, etc.)
-    const mutation = `
-    mutation CreateIssue {
-        createIssue(input: {repositoryId: ${repoId}, title: "TestIssue", body: "Issues exist"}) {
-          issue {
-            number
-            body
+const repoName = 'delete-me';
+const createRepo = `
+    mutation {
+      createRepository(input: {
+        name: "${repoName}",
+        visibility: PRIVATE,
+        description: "Test description",
+        hasIssuesEnabled: true,
+      }) {
+        repository {
+          id
+          name
+          description
+          owner {
+            login
           }
         }
       }
-    `;
-  
-    try {
-      const response = await octokit.graphql({
-        query: mutation,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28',
-          Authorization: `token ${config.token}`,
-        },
-      });
-  
-      console.log('Repository created:', response.repository.id);
-      // Handle the response as needed
-    } catch (error) {
-      console.error('Error:', error.response.data.errors);
-      // Handle errors appropriately
     }
-  }
+  `;
 
-const repoName = 'delete-me';
+async function createRepoAndGetId() {
+    try{
+      const response = await mutate(createRepo);
+      const repoId = response.createRepository.repository.id;
+      console.log("repo id: ", repoId);
+      return repoId;
+    }
+    catch (error) {
+      console.error('Error:', error.message);
+    }
+}
 
-const repository = createRepository(repoName);
-
-repositoryId = repository.id;
-
-createIssue(repositoryId);
+const repoId = createRepoAndGetId();
